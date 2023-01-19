60fe1a2a8e7e533a73799a34f6a261ee51df89ef
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2023-01-19 18:12:30 -0300

    Agregando logica de botones

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css"
 
-const card = ({product, onSelect, type="minusCard"})=>{
+const card = ({product, onSelect, type="minusCard", decreaseQty, increaseQty, numberOfItems})=>{
     const{id, categoryId, description, image, name, price, stock}= product || {};
     
     if (product !== null){
@@ -10,19 +10,34 @@ const card = ({product, onSelect, type="minusCard"})=>{
                 <img className={type === "minusCard" ? "card-image" : "card-image plusCard"} src={image} alt={name} />
                 <div className="card-content">
                     <h2 className="card-title">{name}</h2>
-                    <h2 className="card-desciption">{description}</h2>
+                    {type === "plusCard" && (
+                        <h2 className="card-desciption">{description}</h2>
+                    )} 
                     <p className="card-price">{price}</p>
                     <p  className="card-stock"> {stock} in stock</p>
                 </div>
+                {type === "plusCard" && (
+                    <div className="card-button-container">
+                    <button
+                        disabled={numberOfItems === 0 }
+                        className="card-button-minus" 
+                        onClick={()=> decreaseQty(id)}
 
-                <div className="card-button-container">
-                    <button className="card-button-minus">-</button>
-                    <input type="text" 
-                    className="card-input"
-                    placeholder="0"
+                        >-</button>
+                    <input 
+                        disabled
+                        type="text" 
+                        className="card-input"
+                        value={numberOfItems}
                     />
-                    <button className="card-button-plus">+</button>
+                    <button 
+                        className="card-button-plus" 
+                        onClick={()=> increaseQty(id)}
+                        disabled={numberOfItems === stock}
+                        >+</button>
                 </div>
+                )}
+                
                 
             </div>
             )
--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -1,15 +1,73 @@
 import './styles.css';
-import React from 'react';
+import React, { useState } from 'react';
 import {useLocation } from 'react-router-dom';
 import Card from '../../components/card';
+import { PRODUCTS } from '../../constans/data/products';
+
 
 
 
 const Detail = () => {
     const {state} = useLocation();
+    const [cart, setCart] = useState([]);
+    console.log("cart", cart);
+
+    const onDecreaseItem = (id) => {
+        setCart(currentCart =>{
+            if(currentCart?.find((product)=> product.id === id)?.quantity === 1){
+                return currentCart.filter((product)=> product.id !==  id);
+                
+            }else{
+                return currentCart?.map((product)=>{
+                    if(product.id === id){
+                        return {
+                            ...product,
+                            quantity: product.quantity -1
+                        }
+                    }else {
+                            return product;
+                    }
+                })
+            }
+        })
+    }
+    const onIncreaseItem = (id) => {
+        const item = PRODUCTS.find((product)=> product.id === id);
+        if(cart?.find((product)=> product.id === id)?.quantity === item.stock) return;
+        if(cart?.length === 0){
+            setCart([{...item, quantity: 1}])   
+        }else if(cart.length > 0 && !cart?.find((product)=> product.id === id)){
+            setCart([...cart, {...item, quantity: 1}]);
+        }else{
+            setCart(currentCart => {
+                return currentCart.map((product)=>{
+                    if(product.id === id){
+                        return {...product, quantity: product.quantity + 1}
+                    } else {
+                        return product;
+                    }
+                })
+            })
+        }
+    }
+
+    const getItemQuantity = (id) => {
+        return cart?.find((product) => product.id === id)?.quantity || 0;
+    }
+
     return (
         <div className="detail-container">
-            {state ? (<Card product={state}  key={state.id} onSelect={()=>{} } type="plusCard"/>): null} 
+            {state ? 
+            (<Card 
+                product={state}  
+                key={state.id} 
+                onSelect={()=>{} } 
+                type="plusCard" 
+                decreaseQty={onDecreaseItem} 
+                increaseQty={onIncreaseItem}
+                numberOfItems={getItemQuantity(state?.id)}
+                />)
+                : null} 
             
         </div>
     );
